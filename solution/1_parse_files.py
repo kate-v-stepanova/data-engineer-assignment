36f24cc08cb95e53253c25f05bb02bdbae5d79de
kate-v-stepanova <stephz@yandex.ru>
2023-02-13 20:01:33 +0100

    Parse nanopore data

--- solution/1_parse_files.py
+++ solution/1_parse_files.py
@@ -32,46 +32,56 @@ def illumina(ctx):
     """
     infile = ctx.obj.get('infile')
     dbfile = ctx.obj.get('dbfile')
-    df = pd.read_csv(infile, names=['tree'], engine='python', error_bad_lines=False)
+    table_name = ctx.obj.get('table_name') or 'illumina'
+    df = pd.read_csv(infile, names=['file'], engine='python', error_bad_lines=False)
 
     # remove *trimming_report.txt files
-    df = df.loc[~df['tree'].str.endswith('.txt')]
+    df = df.loc[~df['file'].str.endswith('.txt')]
 
     # remove slurm reports
-    df = df.loc[~df['tree'].str.endswith('trimSlurmReports')]
-    df = df.loc[~df['tree'].str.endswith('.out')]
+    df = df.loc[~df['file'].str.endswith('trimSlurmReports')]
+    df = df.loc[~df['file'].str.endswith('.out')]
 
-    # get level in a tree for each file
-    df['level'] = df['tree'].str.count(' ')
-    df['tree'] = df['tree'].str.split(' ').str[-1]
+    # get level in a file for each file
+    df['level'] = df['file'].str.count(' ')
+    df['file'] = df['file'].str.split(' ').str[-1]
 
     # path for the level 1: dirname (line 0) + filename
-    df['full_path'] = df['tree'][0] + '/'
-    df.loc[df['level'] == 1, 'full_path'] = df.loc[df['level'] == 1, 'full_path'] + df.loc[df['level'] == 1, 'tree']
+    df['full_path'] = df['file'][0] + '/'
+    df.loc[df['level'] == 1, 'full_path'] = df.loc[df['level'] == 1, 'full_path'] + df.loc[df['level'] == 1, 'file']
     df = df.drop(0) # we don't need it anymore
 
     # for the level 2: 
-    # we know, there is just 1 subdir
-    subdir = df.loc[(df['level'] == 1) & (~df['tree'].str.endswith('.gz'))]
-    # files in this subdir
+    # get subdir (there is just one)
+    subdir = df.loc[(df['level'] == 1) & (~df['file'].str.endswith('.gz'))]
+
+    # get files in this subdir
     level2 = df.loc[df['level'] == 2]
-    df.loc[df['level'] == 2, 'full_path'] = subdir['full_path'].iloc[0] + '/' + level2['tree']
+
+    # add full path
+    df.loc[df['level'] == 2, 'full_path'] = subdir['full_path'].iloc[0] + '/' + level2['file']
+
     # remove subdir rows
     df = df.drop(subdir.index)
-   
-    import pdb; pdb.set_trace()
-    df['experiment'] = df['tree'].str.split('_S').str[0]
-    df['sample'] = df['tree'].str.split('_S').str[-1].str.split('_L').str[0]
 
-    # get files with R1 and R2
-    read1 = df.loc[df['tree'].str.contains('_R1_')].drop(['tree', 'level'], axis=1)
-    read2 = df.loc[df['tree'].str.contains('_R2_')].drop(['tree', 'level'], axis=1)
+    # get experiment ID and sample ID (we need experiment ID to merge rows)   
+    df['experiment'] = df['file'].str.split('_S').str[0]
+    df['sample'] = df['file'].str.split('_S').str[-1].str.split('_L').str[0]
 
-    import pdb; pdb.set_trace()
+    # get R1 and R2
+    read1 = df.loc[df['file'].str.contains('_R1_')].drop(['file', 'level'], axis=1)
+    read2 = df.loc[df['file'].str.contains('_R2_')].drop(['file', 'level'], axis=1)
+
+    # merge into one table
     df = read1.merge(read2, on=['experiment', 'sample'])
+
+    # re-arrange cols
     df.columns = ['read1', 'experiment', 'sample', 'read2']
     df = df[['experiment', 'sample', 'read1', 'read2']]
     df = df.drop('experiment', axis=1)
+
+    # open / create database file
+    conn = sqlite3.connect(dbfile)
 
     # write sql
     col_types = ['integer', 'text', 'text']
@@ -87,7 +97,46 @@ def nanopore(ctx):
     """
     parse nanopore data
     """
-    pass
+    infile = ctx.obj.get('infile')
+    dbfile = ctx.obj.get('dbfile')
+    table_name = ctx.obj.get('table_name') or 'nanopore'
+    df = pd.read_csv(infile, names=['file'], engine='python', error_bad_lines=False)
+
+    # get level
+    df['level'] = df['file'].str.count(' ')
+
+    # trim string - take everything after space
+    df['file'] = df['file'].str.split(' ').str[-1]
+
+    # get 0-level (directories)
+    level0 = df.loc[df['level'] == 0].copy()
+
+    # get start and end index for the files in each directory
+    level0['start'] = level0.index
+    level0['end'] = level0.index.tolist()[1:] + [len(df)-1]
+
+    # 
+    for i, row in level0.iterrows():
+        seq_date =  row['file'].split('/')[1].split('_')[0]
+        seq_date = seq_date[-2:] + '.' + seq_date[-4:-2] + '.' + seq_date[:4]
+        df.loc[row['start']:row['end'], 'full_path'] = row['file'] + '/' + df.loc[row['start']:row['end'], 'file']
+        df.loc[row['start']:row['end'], 'seq_date'] = seq_date
+
+    # remove dir rows 
+    df = df.drop(level0.index)
+
+    # re-arrange cols
+    df = df[['seq_date', 'full_path']]
+
+    # open / create database file
+    conn = sqlite3.connect(dbfile)
+
+    # save to sql
+    col_types = ['text', 'date']
+    df.to_sql(table_name, conn, dtypes=col_types)
+    conn.close()
+    print('Data from {} has been saved into DB: {}.{}'.format(infile, dbfile, table_name))
+
 
 # parse sample table
 @cli.command()
